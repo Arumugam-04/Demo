bca371d26ff2f0f8388399d4ed76eb183345d54a
Arumugam-04 <arumugamgkbsm@gmail.com>
2024-03-22 12:03:25 +0530

    updated

---
+++ script.js
@@ -0,0 +1,16 @@
+console.log("hello world")
+
+
+
+
+var person = {
+    name:"aru",
+    email:"arumugamgkbsm",
+    number:1234, 
+
+}
+console.log(person);
+for (var i=0:i<person.length:i=i+1)
+{
+    console.log(var[i]);
+}
